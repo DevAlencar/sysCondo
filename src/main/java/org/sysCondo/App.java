cddefcebbbb16b88b11d82d87a7967785df65dc8
Stharley <sstharley@gmail.com>
2024-10-19 20:59:10 -0300

    feat(controller): add VehicleController

--- src/main/java/org/sysCondo/App.java
+++ src/main/java/org/sysCondo/App.java
@@ -4,9 +4,14 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.hibernate.Session;
 import org.sysCondo.controller.UnitResidentialController;
 import org.sysCondo.controller.UserController;
+import org.sysCondo.controller.VehicleController;
 import org.sysCondo.infra.HibernateUtil;
 import org.sysCondo.model.ownerResidential.OwnerResidential;
+import org.sysCondo.model.user.User;
 import org.sysCondo.model.user.UserRole;
+import org.sysCondo.model.vehicle.BrandEnum;
+
+import javax.validation.constraints.Null;
 
 public class App {
     public static void main(String[] args) {
@@ -15,15 +20,26 @@ public class App {
         //UserController userController = new UserController();
         //userController.createUser("Ricardo", "(12)934567890", "123456789", UserRole.USER);
 
-        UnitResidentialController unitResidentialController = new UnitResidentialController();
-        OwnerResidential owner1 = new OwnerResidential();
+        //UnitResidentialController unitResidentialController = new UnitResidentialController();
+        //OwnerResidential owner1 = new OwnerResidential();
 
-        owner1.setName("Ricardo");
-        owner1.setDocument("sbrubles");
-        owner1.setOwner_id(1L);
+        //owner1.setName("Ricardo");
+        //owner1.setDocument("sbrubles");
+        //owner1.setOwner_id(1L);
 
-        owner1.setUnitsResidentials(null);
+        //owner1.setUnitsResidentials(null);
 
-        unitResidentialController.createUnitResidential(2500, owner1);
+        //unitResidentialController.createUnitResidential(2500, owner1);
+
+        User usuario1 = new User();
+        usuario1.setName("John Doe");
+        usuario1.setContact("123456789");
+        usuario1.setDocument("AB123456");
+        usuario1.setRole(UserRole.USER);
+        usuario1.setId(2L);
+
+        VehicleController vehicleController = new VehicleController();
+        vehicleController.createVehicle("ABC-1234", BrandEnum.VOLKSWAGEN, usuario1);
+
     }
 }
---
+++ src/main/java/org/sysCondo/controller/VehicleController.java
@@ -0,0 +1,98 @@
+package org.sysCondo.controller;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.sysCondo.infra.HibernateUtil;
+import org.sysCondo.model.user.User;
+import org.sysCondo.model.vehicle.Vehicle;
+import org.sysCondo.model.vehicle.BrandEnum;
+
+import java.util.List;
+
+public class VehicleController {
+
+    public void createVehicle(String number, BrandEnum brand, User user) {
+        Session session = HibernateUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            Vehicle vehicle = new Vehicle();
+            vehicle.setNumber(number);
+            vehicle.setBrand(brand);
+            vehicle.setFkUser(user);
+
+            session.save(vehicle);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public Vehicle getVehicleById(Long vehicleId) {
+        Session session = HibernateUtil.getSession();
+        Vehicle vehicle = null;
+        try {
+            vehicle = session.get(Vehicle.class, vehicleId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return vehicle;
+    }
+
+    public List<Vehicle> getAllVehicles() {
+        Session session = HibernateUtil.getSession();
+        List<Vehicle> vehicles = null;
+        try {
+            vehicles = session.createQuery("from Vehicle", Vehicle.class).list();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return vehicles;
+    }
+
+    public void updateVehicle(Long vehicleId, String newNumber, BrandEnum newBrand, User newUser) {
+        Session session = HibernateUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            Vehicle vehicle = session.get(Vehicle.class, vehicleId);
+            if (vehicle != null) {
+                vehicle.setNumber(newNumber);
+                vehicle.setBrand(newBrand);
+                vehicle.setFkUser(newUser);
+                session.update(vehicle);
+                transaction.commit();
+            }
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public void deleteVehicle(Long vehicleId) {
+        Session session = HibernateUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            Vehicle vehicle = session.get(Vehicle.class, vehicleId);
+            if (vehicle != null) {
+                session.delete(vehicle);
+                transaction.commit();
+            }
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+}
